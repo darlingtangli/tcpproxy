a02aea9bd0c28ee94c32c0a852794078a0a75309
Tang Li <tangli@TangdeMacBook-Pro.local>
2018-05-19 01:33:56 +0800

    refactor: use io.Copy

--- tcpproxy.go
+++ tcpproxy.go
@@ -1,65 +1,29 @@
-/**
-* @file tcpproxy.go
-* @brief
-* @author darlintangli@126.com
-* @version 1.0
-* @date 2018-05-18
- */
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 	"os"
 )
 
-func handle(c net.Conn, remoteAddr string) {
-	remoteConn, err := net.Dial("tcp4", remoteAddr)
-	if err != nil {
-		log.Fatalf("dail remote addr %s fail: %s", remoteAddr, err.Error())
-	}
-	log.Println("connect remote:", remoteConn.RemoteAddr())
+func handle(localConn net.Conn, remoteConn net.Conn) {
+	defer localConn.Close()
+	defer remoteConn.Close()
+	done := make(chan struct{})
 
-	writeHandlerClosed := make(chan struct{})
-	// handle write
+	// handle upstream
 	go func() {
-		defer close(writeHandlerClosed)
-		remoteReadBuf := make([]byte, 10240)
-		for {
-			nUpsteam, err := remoteConn.Read(remoteReadBuf)
-			if err != nil {
-				log.Printf("read remote fail: %s\n", err.Error())
-				break
-			}
-			nUpsteam, err = c.Write(remoteReadBuf[:nUpsteam])
-			if err != nil {
-				log.Printf("upstream fail: %s\n", err.Error())
-				break
-			}
-			log.Printf("upstream %d bytes <-- remoteAddr %s\n", nUpsteam, remoteAddr)
-		}
-		log.Println("write handler exit...")
+		defer close(done)
+		nUpsteam, _ := io.Copy(localConn, remoteConn)
+		log.Printf("upstream %d bytes: %v <--  %v\n", nUpsteam, localConn.RemoteAddr(), remoteConn.RemoteAddr())
 	}()
 
-	// handle read
-	localReadBuf := make([]byte, 10240)
-	for {
-		nDownstream, err := c.Read(localReadBuf)
-		if err != nil {
-			log.Printf("read local fail: %s\n", err.Error())
-			break
-		}
-		nDownstream, err = remoteConn.Write(localReadBuf[:nDownstream])
-		if err != nil {
-			log.Printf("downstream fail: %s\n", err.Error())
-			break
-		}
-		log.Printf("downstream %d bytes --> remote addr %s\n", nDownstream, remoteAddr)
-	}
-	log.Println("read handler exit...")
-	c.Close()
-	remoteConn.Close()
-	<-writeHandlerClosed
+	// handle downstream
+	nDownstream, _ := io.Copy(remoteConn, localConn)
+	log.Printf("downstream %d bytes: %v -->  %v\n", nDownstream, localConn.RemoteAddr(), remoteConn.RemoteAddr())
+
+	<-done
 
 	return
 }
@@ -70,19 +34,30 @@ func main() {
 	}
 
 	localAddr := ":" + os.Args[1]
+	remoteAddr := os.Args[2] + ":" + os.Args[3]
+
 	l, err := net.Listen("tcp4", localAddr)
 	if err != nil {
 		log.Fatalf("bind %s fail: %s\n", localAddr, err.Error())
 	}
 
-	remoteAddr := os.Args[2] + ":" + os.Args[3]
 	for {
-		c, err := l.Accept()
+		localConn, err := l.Accept()
 		if err != nil {
-			log.Fatalf("accept fail: %s\n", err.Error())
+			log.Printf("accept fail: %s\n", err.Error())
+			continue
 		}
-		log.Println("accept:", c.RemoteAddr())
-		go handle(c, remoteAddr)
+		log.Println("accept:", localConn.RemoteAddr())
+
+		remoteConn, err := net.Dial("tcp4", remoteAddr)
+		if err != nil {
+			log.Printf("dail remote addr %s fail: %s", remoteAddr, err.Error())
+			localConn.Close()
+			continue
+		}
+		log.Println("connect remote:", remoteConn.RemoteAddr())
+
+		go handle(localConn, remoteConn)
 	}
 
 	return
